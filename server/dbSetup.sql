c0b0fd4c6fff6dd3b7e3efdfa568d90459b3d369
Autumn Eckley <eckley.autumn@gmail.com>
2024-10-17 15:57:48 -0600

    Recipes table

--- server/dbSetup.sql
+++ server/dbSetup.sql
@@ -1,8 +1,26 @@
-CREATE TABLE IF NOT EXISTS accounts(
-  id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
-  createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
-  updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
-  name varchar(255) COMMENT 'User Name',
-  email varchar(255) UNIQUE COMMENT 'User Email',
-  picture varchar(255) COMMENT 'User Picture'
+CREATE TABLE IF NOT EXISTS accounts (
+    id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
+    createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+    updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+    name varchar(255) COMMENT 'User Name',
+    email varchar(255) UNIQUE COMMENT 'User Email',
+    picture varchar(255) COMMENT 'User Picture'
 ) default charset utf8mb4 COMMENT '';
+
+CREATE TABLE recipes (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+    updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+    title TINYTEXT NOT NULL,
+    instructions TEXT NOT NULL,
+    img TEXT NOT NULL,
+    category ENUM(
+        'breakfast',
+        'lunch',
+        'dinner',
+        'snack',
+        'dessert'
+    ) NOT NULL,
+    creatorId VARCHAR(255),
+    FOREIGN KEY (creatorId) REFERENCES accounts (id) ON DELETE CASCADE
+)
