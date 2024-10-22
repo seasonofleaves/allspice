f29a5b888d39c55ac11c82189809632380cadc32
Autumn Eckley <eckley.autumn@gmail.com>
2024-10-22 14:31:23 -0600

    Added favorite profile to favorite model

--- server/Models/Favorite.cs
+++ server/Models/Favorite.cs
@@ -8,3 +8,8 @@ public class Favorite
   public int RecipeId { get; set; }
   public string AccountId { get; set; }
 }
+
+public class FavoriteProfile : Profile
+{
+  public int FavoriteId { get; set; }
+}
