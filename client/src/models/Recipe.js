ffbecf20b425686ea3a5d37ff8382ee124a64215
Autumn Eckley <eckley.autumn@gmail.com>
2024-10-24 10:18:14 -0600

    Created recipe model

---
+++ client/src/models/Recipe.js
@@ -0,0 +1,15 @@
+import { Profile } from "./Profile.js"
+
+export class Recipe{
+  constructor(data){
+    this.id = data.id
+    this.createdAt = new Date(data.createdAt)
+    this.updatedAt = new Date(data.updatedAt)
+    this.title = data.title
+    this.instructions = data.instructions
+    this.imgUrl = data.imgUrl
+    this.category = data.category
+    this.creatorId = data.creatorId
+    this.creator = new Profile(data.creator)
+  }
+}
